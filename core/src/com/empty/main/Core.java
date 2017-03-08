5851278b1d6d5f2aa5fd19fefa477944535e48a6
luke roche <lukerochedev@gmail.com>
2017-03-08 11:38:34 -0500

    Update-- Core

--- core/src/com/empty/main/Core.java
+++ core/src/com/empty/main/Core.java
@@ -5,24 +5,40 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
+
 
 public class Core extends ApplicationAdapter {
-	SpriteBatch batch;
-	Texture img;
-	
+	private SpriteBatch batch;
+	private Texture img;
+	private OrthographicCamera camera;
+	private ShapeRenderer renderer;
 	@Override
 	public void create () {
-		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+		camera = new OrthographicCamera();
+		camera.setToOrtho(false, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+
+		renderer = new ShapeRenderer();
+		renderer.setAutoShapeType(true);
+
 	}
 
 	@Override
 	public void render () {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
+		camera.update();
+		renderer.setProjectionMatrix(camera.combined);
+
+
+		Gdx.gl.glClearColor(0, 0, 0, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		batch.begin();
-		batch.draw(img, 0, 0);
-		batch.end();
+
+
+		renderer.begin();
+		renderer.rect(0.0f, 0.0f, 100.0f, 100.0f);
+		renderer.end();
+
+
 	}
 	
 	@Override
