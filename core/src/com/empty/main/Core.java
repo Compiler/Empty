3ab2703650fd73b4c195aacf4ac4fd1dd44fd9e6
luke roche <lukerochedev@gmail.com>
2017-04-19 08:56:05 -0400

    Update-- Core

--- core/src/com/empty/main/Core.java
+++ core/src/com/empty/main/Core.java
@@ -8,16 +8,14 @@ import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.glutils.ShaderProgram;
 import com.badlogic.gdx.graphics.g2d.Sprite;
-
+import com.badlogic.gdx.graphics.glutils.*;
 public class Core extends ApplicationAdapter {
 	private SpriteBatch batch;
 	private Texture img;
 	private OrthographicCamera camera;
 
-
-	private ShaderProgram shader;
 	private Sprite sprite;
-
+	private ShapeRenderer render;
 	@Override
 	public void create () {
 
@@ -30,29 +28,23 @@ public class Core extends ApplicationAdapter {
 	
 		img = new Texture(Gdx.files.internal("badlogic.jpg"));
 		sprite = new Sprite(img);
-		sprite.setSize(Gdx.graphics.getWidth()/2, Gdx.graphics.getHeight() / 2);
-		sprite.setPosition(100, 100);
-		String one = Gdx.files.internal("Shaders/pass.vert").readString();
-		shader = new ShaderProgram(Gdx.files.internal("Shaders/pass.vert"), Gdx.files.internal("Shaders/pass.frag"));
-		System.out.println(one);
 
-		System.out.println(shader.isCompiled() ? "Compiled successfully" : "Compiled with cancer:\n" + shader.getLog());
+		render = new ShapeRenderer();
+		render.setAutoShapeType(true);
+
 	}
 
 	@Override
 	public void render () {
+		//update
 		camera.update();
 		batch.setProjectionMatrix(camera.combined);
 
 
+		//render
 		Gdx.gl.glClearColor(0, 0, 0, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		
-		batch.setShader(shader);
-		batch.begin();
-		batch.draw(sprite, sprite.getX(), sprite.getY(), sprite.getWidth(), sprite.getHeight());	
-		batch.end();
-
 	}
 	
 	@Override
